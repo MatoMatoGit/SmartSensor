8083a55274d1a731935465499d6ddba438554d05
Dorus van de Spijker <dorus.van.de.spijker@prodrive-technologies.com>
2019-12-10 23:54:51 +0100

    + src/UserInterface/UserButton: Button rest timer and callback, to count button pressed before executing.

--- src/UserInterface/UserButton.py
+++ src/UserInterface/UserButton.py
@@ -1,23 +1,35 @@
 from upyiot.drivers.Switches.TactSwitch import TactSwitch
-
+from machine import Timer
+from micropython import const
 
 class UserButton:
+
+    REST_TIME_MS = const(500)
 
     def __init__(self, btn_pin_nr):
         self.ButtonCbs = (self._ButtonPressed, self._ButtonReleased, self._ButtonHold)
         self.Button = TactSwitch(btn_pin_nr, self.ButtonCbs, 2000, True)
-
+        self.RestTimer = Timer(-1)
+        self.PressCount = 0
         return
 
-    @staticmethod
-    def _ButtonPressed(cb_type):
+    def _ButtonPressed(self, cb_type):
+        self.RestTimer.init(period=UserButton.REST_TIME_MS,
+                            mode=Timer.ONE_SHOT,
+                            callback=UserButton._ButtonRest)
         return
 
-    @staticmethod
-    def _ButtonReleased(cb_type):
+    def _ButtonReleased(self, cb_type):
+        self.RestTimer.deinit()
+        self.PressCount += 1
         return
 
     @staticmethod
     def _ButtonHold(cb_type):
+
         return
 
+    @staticmethod
+    def _ButtonRest():
+        UserButton.PressCount = 0
+        return
