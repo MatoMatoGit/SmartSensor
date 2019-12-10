dab3e2694ad2ed24ae3b51d8a3023473668bc47a
Dorus <spijker.dorus@gmail.com>
2019-12-10 23:03:19 +0100

    + src/Config/Hardware/Pins: Pin configuration.

---
+++ src/Config/Hardware/Pins.py
@@ -0,0 +1,26 @@
+from micropython import const
+
+
+CFG_HW_PIN_VSENSOR_EN           = const(0)
+
+CFG_HW_PIN_TEMP                 = const(32)
+
+CFG_HW_PIN_PHOTO                = const(33)
+
+CFG_HW_PIN_CAP_MOIST_PULSE      = const(34)
+
+CFG_HW_PIN_BTN_0                = const(27)
+
+CFG_HW_PIN_LED_GREEN            = const(22)
+
+CFG_HW_PIN_LED_BLUE             = const(23)
+
+CFG_HW_PIN_LED_RED              = const(21)
+
+CFG_HW_PIN_BAT_STAT             = const(19)
+
+CFG_HW_PIN_VBAT_MEAS_EN         = const(2)
+
+CFG_HW_PIN_VBAT_MEAS            = const(4)
+
+
