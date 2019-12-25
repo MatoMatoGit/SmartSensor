cb4512fbe032b5ca2a16a2fb64e9a43300269505
Dorus <spijker.dorus@gmail.com>
2019-12-25 17:49:14 +0100

    + src/UserInterface/Notification: Classes NotificationRangen, Notification(Observer). Use color mapping params to map value to colors.

--- src/UserInterface/Notification.py
+++ src/UserInterface/Notification.py
@@ -3,27 +3,58 @@ from upyiot.drivers.Led.RgbLed import RgbLed
 from upyiot.drivers.Led.Led import Led
 from upyiot.system.Service.Service import Service
 from micropython import const
-
-MOIST_VALUE_MAX = const(800)
-MOIST_VALUE_MIN = const(20)
+import utime
 
 
-class Notification:
+class NotificationRange:
 
-    STATUS_UNSET    = const(0)
-    STATUS_SET      = const(1)
-    STATUS_ACTIVE   = const(2)
-
-    def __init__(self, subject_obj, threshold, map_param, colors, prio):
-        self.Status = Notification.STATUS_UNSET
-        return
+    def __init__(self, min, max):
+        self.Min = min
+        self.Max = max
 
 
-class Notifyer(Observer, Service):
+class Notification(Observer):
+
+    STATE_UNSET    = const(0)
+    STATE_SET      = const(1)
+    STATE_ACTIVE   = const(2)
+
+    PRIO_MIN        = const(0)
+    PRIO_MAX        = const(100)
+
+    def __init__(self, val_threshold_range, color_map_params, prio, on_time_sec):
+        """
+
+        :param val_threshold_range: Range object with min and max threshold values.
+        :param color_map_params: Dictionary containing a Range object as value for each
+        RGB_COLOR_* key.
+        :param prio: Notification priority. If multiple notifications are active, the highest
+        priority is shown first.
+        :param on_time_sec: Amount of time the LED is on for.
+        """
+        self.State = Notification.STATE_UNSET
+        self.Value = 0
+        self.ThresholdRange = val_threshold_range
+        self.ColorMapParams = color_map_params
+        self.Prio = prio
+        self.OnTimeSec = on_time_sec
+
+    def Update(self, arg):
+        self.Value = arg
+        if self.State is Notification.STATE_UNSET:
+            if self.Value >= self.ThresholdRange.Min and \
+            self.Value <= self.ThresholdRange.Max:
+                self.State = self.STATE_SET
+        else:
+            if self.Value < self.ThresholdRange.Min or \
+                    self.Value > self.ThresholdRange.Max:
+                self.State = self.STATE_UNSET
+
+
+class Notifyer(Service):
     
     def __init__(self, rgbled_obj):
         super().__init__()
-        self.MoistureLevel = 0
         self.RgbLed = rgbled_obj
         self.Notifications = set()
         self.ActiveNotification = None
@@ -32,9 +63,39 @@ class Notifyer(Observer, Service):
         return
 
     def SvcRun(self):
-        self.RgbLed.Color(self._MapMoistureToRgb(
-            self.Moisture.MoistureLevel))
-        return
+        if self.ActiveNotification.State is Notification.STATE_UNSET:
+            self.ActiveNotification.Prio = self.PRIO_MIN
+
+        for notf in self.Notifications:
+            if notf.State is Notification.STATE_SET:
+                if notf.Prio > self.ActiveNotification.Prio:
+                    self.ActiveNotification = notf
+
+        self.ActiveNotification.State = Notification.STATE_ACTIVE
+        if RgbLed.RGB_COLOR_RED in self.ActiveNotification.ColorMapParams.keys():
+            red_val = Notifyer.MapValue(self.ActiveNotification.Value,
+                                        self.ActiveNotification.ColorMapParams[RgbLed.RGB_COLOR_RED].Min,
+                                        self.ActiveNotification.ColorMapParams[RgbLed.RGB_COLOR_RED].Max)
+        else:
+            red_val = 0
+
+        if RgbLed.RGB_COLOR_GREEN in self.ActiveNotification.ColorMapParams.keys():
+            green_val = Notifyer.MapValue(self.ActiveNotification.Value,
+                                          self.ActiveNotification.ColorMapParams[RgbLed.RGB_COLOR_GREEN].Min,
+                                          self.ActiveNotification.ColorMapParams[RgbLed.RGB_COLOR_GREEN].Max)
+        else:
+            green_val = 0
+
+        if RgbLed.RGB_COLOR_BLUE in self.ActiveNotification.ColorMapParams.keys():
+            blue_val = Notifyer.MapValue(self.ActiveNotification.Value,
+                                         self.ActiveNotification.ColorMapParams[RgbLed.RGB_COLOR_BLUE].Min,
+                                         self.ActiveNotification.ColorMapParams[RgbLed.RGB_COLOR_BLUE].Max)
+        else:
+            blue_val = 0
+
+        self.RgbLed.ColorsSet(red_val, green_val, blue_val)
+        utime.sleep(self.ActiveNotification.OnTimeSec)
+        self.RgbLed.Off()
 
     def NotificationSet(self, notification):
         notification.Status = Notification.STATUS_SET
@@ -45,40 +106,10 @@ class Notifyer(Observer, Service):
         return
 
     def NotificationRegister(self, notification):
-        """
-
-        :param subject_obj:
-        :param map_param: Tuple containing the input's minimum and maximum value.
-        :param colors: Tuple containing the color
-        :param prio:
-        :return:
-        """
         self.Notifications.add(notification)
         return
 
-    def Update(self, arg):
-        self.MoistureLevel = arg
-
-    def NotifyLowBattery(self):
-        return 0
-
     @staticmethod
-    def _MapMoistureToRgb(moisture):
-        red_val = Notifyer.MapValue(moisture,
-                                    MOIST_VALUE_MIN,
-                                    MOIST_VALUE_MAX,
-                                    Led.PWM_DUTY_MAX,
-                                    Led.PWM_DUTY_MIN)
-        blue_val = Notifyer.MapValue(moisture,
-                                     MOIST_VALUE_MIN,
-                                     MOIST_VALUE_MAX,
-                                     Led.PWM_DUTY_MIN,
-                                     Led.PWM_DUTY_MAX)
-
-        green_val = 0
-        return red_val, green_val, blue_val
-
-    @staticmethod
-    def MapValue(value, in_min, in_max, out_min, out_max):
+    def MapValue(value, in_min, in_max, out_min=Led.PWM_DUTY_MIN, out_max=Led.PWM_DUTY_MAX):
         return out_min + (out_max - out_min) * ((value - in_min) / (in_max - in_min))
 
