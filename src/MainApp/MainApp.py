50d0ce6192140a7e2c14b867535040b97f8428bc
Dorus <spijker.dorus@gmail.com>
2020-02-02 16:38:53 +0100

    + src/MainApp/MainApp: Using UserButton, RgbLed and Notification.

--- src/MainApp/MainApp.py
+++ src/MainApp/MainApp.py
@@ -4,7 +4,6 @@ from upyiot.system.ExtLogging import ExtLogging
 from upyiot.system.SystemTime.SystemTime import SystemTime
 from upyiot.system.Service.ServiceScheduler import ServiceScheduler
 from upyiot.system.Service.ServiceScheduler import Service
-# from upyiot.system.Power.BatteryMonitor import BatteryMonitor
 from upyiot.comm.NetCon.NetCon import NetCon
 from upyiot.comm.Messaging.MessageExchange import MessageExchange
 from upyiot.comm.Messaging.MessageExchange import Endpoint
@@ -15,15 +14,17 @@ from upyiot.middleware.Sensor import Sensor
 from upyiot.drivers.Sensors.CapMoisture import CapMoisture
 from upyiot.drivers.Sensors.Mcp9700Temp import Mcp9700Temp
 from upyiot.drivers.Sensors.PhTLight import PhTLight
-# from upyiot.drivers.Sensors.BatteryLevel import BatteryLevel
-from upyiot.drivers.Led.Led import Led
+from upyiot.drivers.Led.RgbLed import RgbLed
 
 # SmartSensor modules
 from Messages.LogMessage import LogMessage
 from Messages.SensorReport import SensorReportTemp
 from Messages.SensorReport import SensorReportMoist
 from Messages.SensorReport import SensorReportLight
-# from Messages.SensorReport import SensorReportVBat
+from UserInterface.Notification import Notifyer
+from UserInterface.Notification import NotificationRange
+from UserInterface.Notification import Notification
+from UserInterface.UserButton import UserButton
 from Config.Hardware import Pins
 
 # micropython modules
@@ -42,14 +43,16 @@ class MainApp:
     DIR = "./"
     ID = str(ubinascii.hexlify(machine.unique_id()).decode('utf-8'))
     RETRIES = 3
-    BROKER = '192.168.0.103'
+    BROKER = '192.168.0.111'  # '192.168.0.103'
     PORT = 1883
     FILTER_DEPTH = const(20)
     DEEPSLEEP_THRESHOLD_SEC = const(5)
 
-    MqttClient = None
-    MsgEx = None
-    Time = None
+    MOIST_RANGE_MIN = const(1000)
+    MOIST_RANGE_MAX = const(10000)
+    MOIST_NOTIF_TIME = const(3)
+    MOIST_NOTIF_PRIO = const(5)
+
     NetCon = None
     UrlFields = {MessageSpecification.URL_FIELD_DEVICE_ID: ID,
                  MessageSpecification.URL_FIELD_PRODUCT_NAME: PRODUCT_NAME}
@@ -57,9 +60,9 @@ class MainApp:
     SamplesPerMessage   = const(1)
 
     # Service intervals in seconds.
-    MsgExInterval           = const(60)
-    EnvSensorReadInterval   = const(10)
-    VBatSensorReadInterval  = const(20)
+    MsgExInterval           = const(35)
+    EnvSensorReadInterval   = const(7)
+    NotificationInterval    = const(7)
 
     def __init__(self, netcon_obj):
         # Configure the URL fields.
@@ -69,16 +72,16 @@ class MainApp:
 
     def Setup(self):
 
-        # Create sensor driver instances.
-        self.CapMoist = CapMoisture(Pins.CFG_HW_PIN_CAP_MOIST_PULSE, Pins.CFG_HW_PIN_VSENSOR_EN)
-        self.Mcp9700Temp = Mcp9700Temp(Pins.CFG_HW_PIN_TEMP)
-        self.PhTLight = PhTLight(Pins.CFG_HW_PIN_PHOTO)
+        # Create driver instances.
+        self.CapMoist = CapMoisture(Pins.CFG_HW_PIN_MOIST, Pins.CFG_HW_PIN_MOIST_EN)
+        self.Mcp9700Temp = Mcp9700Temp(Pins.CFG_HW_PIN_TEMP, Pins.CFG_HW_PIN_TEMP_EN)
+        self.Ldr = PhTLight(Pins.CFG_HW_PIN_LDR, Pins.CFG_HW_PIN_LDR_EN)
         # self.VBat = BatteryLevel(1, Pins.CFG_HW_PIN_VBAT_MEAS, Pins.CFG_HW_PIN_VBAT_MEAS_EN)
 
-        # Create LED driver instances.
-        self.LedRed = Led(Pins.CFG_HW_PIN_LED_RED)
-        self.LedGreen = Led(Pins.CFG_HW_PIN_LED_GREEN)
-        self.LedBlue = Led(Pins.CFG_HW_PIN_LED_BLUE)
+        self.RgbLed = RgbLed(Pins.CFG_HW_PIN_LED_RED,
+                             Pins.CFG_HW_PIN_LED_GREEN, Pins.CFG_HW_PIN_LED_BLUE)
+
+        self.Button = UserButton(Pins.CFG_HW_PIN_BTN_0)
 
         # Create the WLAN station interface.
         wlan_ap = WLAN(network.STA_IF)
@@ -88,21 +91,18 @@ class MainApp:
 
         self.TempSensor = Sensor.Sensor(self.DIR,
                                         SensorReportTemp.NAME_TEMP,
-                                        self.FILTER_DEPTH, self.Mcp9700Temp)
+                                        self.FILTER_DEPTH, self.Mcp9700Temp,
+                                        self.FILTER_DEPTH)
 
         self.MoistSensor = Sensor.Sensor(self.DIR,
                                          SensorReportMoist.NAME_MOIST,
-                                         self.FILTER_DEPTH, self.CapMoist)
+                                         self.FILTER_DEPTH, self.CapMoist,
+                                         self.FILTER_DEPTH)
 
         self.LightSensor = Sensor.Sensor(self.DIR,
                                          SensorReportLight.NAME_LIGHT,
-                                         self.FILTER_DEPTH, self.PhTLight)
-
-        # self.VBatSensor = Sensor.Sensor(self.DIR,
-        #                                 SensorReportVbat.NAME_VBAT,
-        #                                 self.FILTER_DEPTH, self.VBat)
-        #
-        # self.VBatMon = BatteryMonitor(self.DIR, self.VBat, 20)
+                                         self.FILTER_DEPTH, self.PhTLight,
+                                         self.FILTER_DEPTH)
 
         self.MqttClient = MQTTClient(self.ID,
                                      self.BROKER,
@@ -111,6 +111,8 @@ class MainApp:
                                      self.MqttClient,
                                      self.ID,
                                      self.RETRIES)
+
+        self.Notifyer = Notifyer(self.RgbLed)
 
         self.MsgEp = Endpoint()
 
@@ -123,6 +125,7 @@ class MainApp:
         self.TempSensor.SvcDependencies({self.Time: Service.DEP_TYPE_RUN_ONCE_BEFORE_RUN})
         self.MoistSensor.SvcDependencies({self.Time: Service.DEP_TYPE_RUN_ONCE_BEFORE_RUN})
         self.LightSensor.SvcDependencies({self.Time: Service.DEP_TYPE_RUN_ONCE_BEFORE_RUN})
+        self.Notifyer.SvcDependencies({self.MoistSensor: Service.DEP_TYPE_RUN_ALWAYS_BEFORE_RUN})
 
         # Register all services to the scheduler.
         self.Scheduler.ServiceRegister(self.Time)
@@ -131,19 +134,18 @@ class MainApp:
         self.Scheduler.ServiceRegister(self.TempSensor)
         self.Scheduler.ServiceRegister(self.MoistSensor)
         self.Scheduler.ServiceRegister(self.LightSensor)
-        # self.Scheduler.ServiceRegister(self.VBatMon.BatteryLevelSensor)
+        self.Scheduler.ServiceRegister(self.Notifyer)
 
         # Create message specifications.
         self.TempMsgSpec = SensorReportTemp()
         self.MoistMsgSpec = SensorReportMoist()
         self.LightMsgSpec = SensorReportLight()
         self.LogMsgSpec = LogMessage()
-        # self.VBatMsgSpec = SensorReportVBat()
 
         # Create MessageFormatAdapters and couple them with their message specs.
-        self.TempAdapt = MessageFormatAdapter(self.MsgEp,
-                                              MessageFormatAdapter.SEND_ON_COMPLETE,
-                                              self.TempMsgSpec)
+        # self.TempAdapt = MessageFormatAdapter(self.MsgEp,
+        #                                       MessageFormatAdapter.SEND_ON_COMPLETE,
+        #                                       self.TempMsgSpec)
         self.MoistAdapt = MessageFormatAdapter(self.MsgEp,
                                                MessageFormatAdapter.SEND_ON_COMPLETE,
                                                self.MoistMsgSpec)
@@ -153,16 +155,12 @@ class MainApp:
         self.LogAdapt = MessageFormatAdapter(self.MsgEp,
                                              MessageFormatAdapter.SEND_ON_COMPLETE,
                                              self.LogMsgSpec)
-        # self.VBatAdapt = MessageFormatAdapter(self.MsgEp,
-        #                                       MessageFormatAdapter.SEND_ON_COMPLETE,
-        #                                       self.VBatMsgSpec)
 
         # Register message specs for exchange.
-        self.MsgEx.RegisterMessageType(self.TempMsgSpec)
+        # self.MsgEx.RegisterMessageType(self.TempMsgSpec)
         self.MsgEx.RegisterMessageType(self.MoistMsgSpec)
         self.MsgEx.RegisterMessageType(self.LightMsgSpec)
         self.MsgEx.RegisterMessageType(self.LogMsgSpec)
-        # self.MsgEx.RegisterMessageType(self.VBatMsgSpec)
 
         # Create observers for the sensor data.
         self.TempObserver = self.TempAdapt.CreateObserver(
@@ -174,15 +172,24 @@ class MainApp:
         self.LightObserver = self.LightAdapt.CreateObserver(
             SensorReportLight.DATA_KEY_SENSOR_REPORT_SAMPLES,
             self.SamplesPerMessage)
-        # self.VBatObserver = self.VBatAdapt.CreateObserver(
-        #     SensorReportVBat.DATA_KEY_SENSOR_REPORT_SAMPLES,
-        #     self.SamplesPerMessage)
+
+        # Set the notification ranges and colors.
+        moist_th_range = NotificationRange(self.MOIST_RANGE_MIN, self.MOIST_RANGE_MAX)
+        moist_color_map = {RgbLed.RGB_COLOR_RED: NotificationRange(self.MOIST_RANGE_MAX,
+                                                                   self.MOIST_RANGE_MIN),
+                           RgbLed.RGB_COLOR_BLUE: NotificationRange(self.MOIST_RANGE_MIN,
+                                                                    self.MOIST_RANGE_MAX)}
+        self.MoistNotif = Notification(moist_th_range, moist_color_map,
+                                       self.MOIST_NOTIF_PRIO,
+                                       self.MOIST_NOTIF_TIME)
+
+        self.Notifyer.NotificationRegister(self.MoistNotif)
 
         # Link the observers to the sensors.
         self.TempSensor.ObserverAttachNewSample(self.TempObserver)
         self.MoistSensor.ObserverAttachNewSample(self.MoistObserver)
+        self.MoistSensor.ObserverAttachNewSample(self.MoistNotif)
         self.LightSensor.ObserverAttachNewSample(self.LightObserver)
-        # self.VBatMon.BatteryLevelSensor.ObserverAttachNewSample(self.VBatObserver)
 
         # Create a stream for the log messages.
         self.LogStream = self.LogAdapt.CreateStream(LogMessage.DATA_KEY_LOG_MSG,
@@ -196,7 +203,7 @@ class MainApp:
         self.MoistSensor.SvcIntervalSet(self.EnvSensorReadInterval)
         self.TempSensor.SvcIntervalSet(self.EnvSensorReadInterval)
         self.LightSensor.SvcIntervalSet(self.EnvSensorReadInterval)
-        # self.VBatMon.BatteryLevelSensor.SvcIntervalSet(self.VBatSensorReadInterval)
+        self.Notifyer.SvcIntervalSet(self.NotificationInterval)
 
     def Reset(self):
         self.MsgEx.Reset()
@@ -206,16 +213,4 @@ class MainApp:
         self.NetCon.StationSettingsReset()
 
     def Run(self):
-        self.LedRed.Toggle()
-        utime.sleep(1)
-        self.LedRed.Toggle()
-
-        self.LedGreen.Toggle()
-        utime.sleep(1)
-        self.LedGreen.Toggle()
-
-        self.LedBlue.Toggle()
-        utime.sleep(1)
-        self.LedBlue.Toggle()
-
         self.Scheduler.Run()
