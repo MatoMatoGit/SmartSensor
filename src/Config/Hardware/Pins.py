6af9a56fb127f6d06059f7810e0e7e32d3afa8c1
Dorus <spijker.dorus@gmail.com>
2020-02-08 21:39:56 +0100

    + src/Config/Hardware/Pins: Pins configuration for HW V1.1

--- src/Config/Hardware/Pins.py
+++ src/Config/Hardware/Pins.py
@@ -1,19 +1,40 @@
 from micropython import const
 
-CFG_HW_PIN_TEMP_EN              = const(18)
+# HW 1.1 pins
+
+# CFG_HW_PIN_TEMP_EN              = const(18)
+# CFG_HW_PIN_TEMP                 = const(32)
+#
+# CFG_HW_PIN_LDR_EN               = const(5)
+# CFG_HW_PIN_LDR                  = const(33)
+#
+# CFG_HW_PIN_MOIST_EN             = const(17)
+# CFG_HW_PIN_MOIST                = const(34)
+#
+# CFG_HW_PIN_HI_PWR_EN            = const(16)
+#
+# CFG_HW_PIN_BTN_0                = const(27)
+#
+# CFG_HW_PIN_LED_GREEN            = const(23)
+# CFG_HW_PIN_LED_BLUE             = const(22)
+# CFG_HW_PIN_LED_RED              = const(21)
+
+
+# HW V1.0 pins with V1.1 app
+
+CFG_HW_PIN_TEMP_EN              = const(0)
 CFG_HW_PIN_TEMP                 = const(32)
 
-CFG_HW_PIN_LDR_EN               = const(5)
+CFG_HW_PIN_LDR_EN               = const(0)
 CFG_HW_PIN_LDR                  = const(33)
 
-CFG_HW_PIN_MOIST_EN             = const(17)
+CFG_HW_PIN_MOIST_EN             = const(0)
 CFG_HW_PIN_MOIST                = const(34)
-
-CFG_HW_PIN_HI_PWR_EN            = const(16)
 
 CFG_HW_PIN_BTN_0                = const(27)
 
 CFG_HW_PIN_LED_GREEN            = const(23)
-CFG_HW_PIN_LED_BLUE             = const(22)
-CFG_HW_PIN_LED_RED              = const(21)
 
+CFG_HW_PIN_LED_BLUE             = const(22)
+
+CFG_HW_PIN_LED_RED              = const(21)
